4e5c69e6b3a310264f87c02b7a9e8c21a5855a32
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-10-26 16:01:10 -0500

    Fix TikTok service

--- data/exporter/TiktokService.ts
+++ data/exporter/TiktokService.ts
@@ -6,17 +6,16 @@ export default class TiktokService implements AudioService {
     public id: string = 'TIKTOK'
 
     public async export(text: string, voice: string, outputDir: string): Promise<void> {
-        const data = { text, voice }
-
-        const request = await axios({
-            url: 'https://tiktok-tts.weilnet.workers.dev/api/generation',
-            method: 'post',
+        const data = { text, voice, base64: true }
+    
+        const request = await fetch('https://tiktok-tts.weilbyte.dev/api/generate', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            data
+            body: JSON.stringify(data)
         })
-        const response = request.data
-        ExporterUtils.writeFile(outputDir, response.data)
+        const response = await request.text()
+        ExporterUtils.writeFile(outputDir, response)
     }
 }
