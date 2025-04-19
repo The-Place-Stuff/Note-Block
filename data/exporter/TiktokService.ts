d5e06de469e6d48c4e4e8951798602af70d8f7b7
ExDrill <harblast123@gmail.com>
2025-04-19 13:42:16 -0500

    Fix TikTok service!

--- data/exporter/TiktokService.ts
+++ data/exporter/TiktokService.ts
@@ -5,26 +5,19 @@ import axios from 'axios'
 export default class TiktokService implements AudioService {
     public id: string = 'TIKTOK'
 
-    /*
-        A TikTok session ID that can and will expire, you'll need to login via the website on PC and grab the sessionId cookie.
-        You can use a browser extension to fetch the cookie.
-    */
-    private sessionId: string = 'a5cfee374112f667749b9feaeaf9e970'
-
     public async export(text: string, voice: string, outputDir: string): Promise<void> {
-        let baseUrl = 'https://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/'
-        
-        baseUrl += `?text_speaker=${voice}&`
-        baseUrl += `req_text=${text}&`
-        baseUrl += 'speaker_map_type=0&aid=1233'
+        const body = {
+            text,
+            voice
+        }
     
-        const request = await axios.post(baseUrl, {}, {
+        const request = await axios.post('https://tiktok-tts.weilnet.workers.dev/api/generation', JSON.stringify(body), {
             headers: {
-                'User-Agent': 'com.zhiliaoapp.musically/2022600030 (Linux; U; Android 7.1.2; es_ES; SM-G988N; Build/NRD90M;tt-ok/3.12.13.1)',
-                'Cookie': `sessionid=${this.sessionId}`
+                'Content-Type': 'application/json'
             }
         })
         const response = request.data
-        console.log(response)
+
+        ExporterUtils.writeFile(outputDir, response.data)
     }
 }
